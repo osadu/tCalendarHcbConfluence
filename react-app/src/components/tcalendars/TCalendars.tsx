2b7752bfb8a083cb2564fe64755d379be85cdf9b
osadu <oljas.saduakhas@gmail.com>
2020-09-15 16:42:43 +0600

    add popupOpen method 3

--- react-app/src/components/tcalendars/TCalendars.tsx
+++ react-app/src/components/tcalendars/TCalendars.tsx
@@ -11,8 +11,9 @@ const TCalendar = (props: TCalendarProps) => {
 
     let data: object [] = [{
         Id: 1,
-        Subject: 'Meeting - 1dasdasdasdasdasdasdadasdasdasdasdasfagfdgfh sfsdfsdf',
-        StartTime: new Date(2018, 1, 15, 10, 0),
+        Subject: 'Meeting - 1sadasdasdasdasdasd asfasfafasfsafasf',
+        StartTime: new Date(2020, 8, 15, 10, 0),
+        EndTime: new Date(2020, 8, 15, 10, 0),
         IsAllDay: false,
         Description: "Это прмер описания даноого ивента",
         Status: "Done"
